efe2784da3c4e31d4f1acd6c0f91b052fcbb879e
Radu <radu.stanciu2016@gmail.com>
2024-09-19 20:26:49 +0300

    added toggleDone and togglePriority functions to todo object factory, made basic html template and added some css for the layout and colors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-const todo = function (title, description, dueDate, priority) {
-    return {title, description, dueDate, priority}
+import "./style.css";
+
+const todo = function (title, description, dueDate, important) {
+    const done = false;
+    const toggleDone = () => {
+        done = !done;
+    }
+
+    const togglePriority = () => {
+        important = !important;
+    }
+
+    return {title, description, dueDate, priority, toggleDone, toggleImportance}
 }
+
+let general = [];
+
+
+general.push(todo("read", "30 pages", new Date(), true));
